3f76fbacf6df7aa1db81808fe968161657b6548f
DewetAlexandre <107472316+DewetAlexandre@users.noreply.github.com>
2022-09-29 15:17:21 +0200

    q

--- atypik-house/app/Http/Controllers/HomeController.php
+++ atypik-house/app/Http/Controllers/HomeController.php
@@ -9,15 +9,5 @@ use App\Models\User;
 
 class HomeController extends Controller
 {
-        public function __invoke()
-    {
-        $categories = Categorie_logement::all();
-        $user = User::all();
-        $best4Avis = Avis::where('note','>','3')->orderBy('note','desc')->limit(4)->get();
-        return view('accueil',[
-            'categories' => $categories,
-            'user' => $user,
-            'best4Avis' => $best4Avis
-        ]);
-    }
+
 }
